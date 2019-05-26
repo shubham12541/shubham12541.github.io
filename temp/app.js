c3e685f0af3881712e200aa933eaf2265bc57b66
Shubham Chaudhary <shubham.chaudhary@sap.com>
2019-05-26 22:11:05 +0530

    some changes

--- temp/app.js
+++ temp/app.js
@@ -2,3 +2,8 @@ console.log("Hello");
 
 import {MDCRipple} from '@material/ripple/index';
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
+});
